f90e3e41ad92bc910c43f6da61427d1fd96dbdae
francis2911 <138940663+francis2911@users.noreply.github.com>
2024-02-28 16:01:09 -0400

    mejora de errores

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,41 @@
 import { fetchImages, fetchMoreImages } from './api.js';
+
 import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import Card from './card.js';
 
 const form = document.getElementById('searchForm');
+
 const gallery = document.querySelector('.gallery');
+
 gallery.style.maxWidth = '1440px';
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
+
   let pageNumber = 1;
 
   let q = e.target.elements['searchForm'].value;
+
   if (q.trim() === '') {
     return;
   }
+
+  // Limpiar contenido de la galería antes de agregar nuevas imágenes
+
+  gallery.innerHTML = '';
+
   let data = await fetchImages(q, pageNumber);
+
   data.forEach(image => {
     const card = new Card(image);
+
     gallery.appendChild(card.newCard());
   });
 
-  gallery.addEventListener('click', openModal);
-
-  function openModal(e) {
-    e.preventDefault();
-  }
+  // No es necesario agregar un listener para 'click' en este punto
 
   const lightbox = new SimpleLightbox('.gallery a', {
     captionsDelay: '250ms',
@@ -33,16 +43,22 @@ form.addEventListener('submit', async e => {
 
   window.onscroll = async function () {
     var scrollTop = window.scrollY || document.documentElement.scrollTop;
+
     var windowHeight = window.innerHeight;
+
     var documentHeight = document.documentElement.scrollHeight;
 
     if (scrollTop + windowHeight === documentHeight) {
       pageNumber += 1;
+
       data = await fetchMoreImages(q, pageNumber);
+
       data.forEach(image => {
         const card = new Card(image);
+
         gallery.appendChild(card.newCard());
       });
+
       lightbox.refresh();
     }
   };
